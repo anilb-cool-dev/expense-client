9f3c459eb07e20085456403cc25529bff6206c1b
EC2 Default User <ec2-user@ip-172-31-35-130.us-west-2.compute.internal>
2017-04-18 17:04:06 +0000

    fix

--- src/main/java/com/concur/expense/ReportsList.java
+++ src/main/java/com/concur/expense/ReportsList.java
@@ -11,7 +11,7 @@ public class ReportsList
         List l = new ArrayList();
 
         String json = getRestResponse();
-        String name = "testing 123...";
+        String name = json;
 
         l.add(name);
         return l;
@@ -30,14 +30,15 @@ public class ReportsList
 
         try
         {
-            URL url = new URL("https://qaers.concurasp.com/expensereports/v4/users/FB3816AE-8C07-4402-B36B-F773E440ED83/context/TRAVELER/reports");
+            URL url = new URL("https://us.api.concursolutions.com/expensereports/v4/users/378D7F81-BEA3-4D88-9E40-05A8BDF7A8DF/context/TRAVELER/reports");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
-            conn.setRequestProperty("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjE0NTU2MTQzNDYifQ.eyJodHRwczovL2FwaS5jb25jdXJzb2x1dGlvbnMuY29tL3Njb3BlcyI6WyIqIl0sInN1YiI6IkZCMzgxNkFFLThDMDctNDQwMi1CMzZCLUY3NzNFNDQwRUQ4MyIsImF1ZCI6IioiLCJpc3MiOiJodHRwczovL3VzLmFwaS5jb25jdXJzb2x1dGlvbnMuY29tIiwiZXhwIjoxNDY0NzY4MTY1LCJuYmYiOjE0NjQ3NjA5NjUsImlhdCI6MTQ2NDc2MDk2NX0.QtyaV7bPiudrZ-MErVTbzRfa867TQzQihh_VZkfuo9N1ndSUzZ6QMLvwO59-edTZbt1dJmiM0d-LxHr5tm1kq8I85dZ0rFvqi-tFtHgI6-jxjaluoZkRtbMzGG_YsW19FkyjZHcsEfQC349OUZo2K_U-SG40GOoVInBYVXYM-KUiMwwYa4ngYT64fV53LZTwdtUIHCFDI6DEmOVgvN0s1VBW3SXgWgZKQgQ43bCOdc1UjASNxt-EurL4o4OPYMA_7Qr75-54UnB-5TAW7rgfKGU3AxfjIqBV-fVsZldjt7pgV2wHSE2MQC4EMMlbHrYWR-3on7_-x-e-7VzVfWAhdQ");
+            conn.setRequestProperty("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjE0NTU2MTQwMjIifQ.eyJjb25jdXIuc2NvcGVzIjpbIlVTRVIiLCJwcm9maWxlIiwidXNlcl9yZWFkIl0sImF1ZCI6IioiLCJjb25jdXIuYXBwSWQiOiJlOTQzM2IzNi0xYjAxLTQ4MWEtOTM5MC1kM2NhNDY2ZDJmYzgiLCJzdWIiOiIzNzhkN2Y4MS1iZWEzLTRkODgtOWU0MC0wNWE4YmRmN2E4ZGYiLCJpc3MiOiJodHRwczovL3VzLmFwaS5jb25jdXJzb2x1dGlvbnMuY29tIiwiY29uY3VyLnByb2ZpbGUiOiJodHRwczovL3VzLmFwaS5jb25jdXJzb2x1dGlvbnMuY29tL3Byb2ZpbGUvdjEvcHJpbmNpcGFscy8zNzhkN2Y4MS1iZWEzLTRkODgtOWU0MC0wNWE4YmRmN2E4ZGYiLCJleHAiOjE0OTI1MzY0MTEsImxlZ2FjeV9hcHBsaWNhdGlvbl9pZCI6ODI1NiwiY29uY3VyLnZlcnNpb24iOjIsImNvbmN1ci50eXBlIjoidXNlciIsImNvbmN1ci5hcHAiOiJodHRwczovL3VzLmFwaS5jb25jdXJzb2x1dGlvbnMuY29tL3Byb2ZpbGUvdjEvYXBwcy9lOTQzM2IzNi0xYjAxLTQ4MWEtOTM5MC1kM2NhNDY2ZDJmYzgiLCJuYmYiOjE0OTI1MzI4MTEsImlhdCI6MTQ5MjUzMjgxMX0.nPvBiceIk8Ckj2MPAJ-ZiUv7lSFq_bCltTefsNUu7d90zz6s5BUdHx5K3eUyOBGsqWp9sHHTHEgVmhw4GB5CNNdTkj-YSNXPTXtBuGK3FOvfZr9fNq4WSnexreCocyoNQRdAhjf_MRirRnKirOxyApU-JVBkHq58GdBxY0lbcY2qEEpqXbf6teMEoxsNV8fZ4Axm-Af44u4x1lsf3UEZqmFE3z-P20Q2ttdZCeZo0O7DyAqor30UsPSbUONG6jOFUibja_ReTYXLnuPGIq05sKCpjxkPbqF_N-B3dYhEiPesV3HXO4K-NaaL8AvfXoyMX63FaUZZp0Lg3akA1ObPYw");
 
             if (conn.getResponseCode() != 200)
             {
+json = "Sorry";
                 throw new RuntimeException("Failed : HTTP error code : "
                         + conn.getResponseCode());
             }
@@ -55,10 +56,14 @@ public class ReportsList
         }
         catch (MalformedURLException e)
         {
+json = "Sorry1";
             e.printStackTrace();
         }
         catch (IOException e)
         {
+StringWriter errors = new StringWriter();
+e.printStackTrace(new PrintWriter(errors));
+json = errors.toString();
             e.printStackTrace();
         }
 
